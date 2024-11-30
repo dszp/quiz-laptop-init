b9d5157823b9b65cc9ebac3403e7c2022fd66ef1
David Szpunar <dszp@users.noreply.github.com>
2024-11-30 17:13:09 -0500

    disable sleep and standby on AC power

--- system-prep.ps1
+++ system-prep.ps1
@@ -3,6 +3,10 @@
  
 REG ADD "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager\Power" /v HiberbootEnabled /t REG_DWORD /d "0" /f
 
+# Disable sleep and standby when on AC power:
+Powercfg /Change monitor-timeout-ac 0
+Powercfg /Change standby-timeout-ac 0
+
 # Harden Windows Registry
 iex (irm "https://raw.githubusercontent.com/dszp/MSP-Scripts/refs/heads/main/Windows-Security-Hardening/Harden-Security-Windows-Registry.ps1")
 
