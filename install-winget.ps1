a5a9736d44f54af5b94773ffaf7066fe6f5813a2
David Szpunar <dszp@users.noreply.github.com>
2024-11-30 15:46:53 -0500

    winget accept source agreements to stop prompt

--- install-winget.ps1
+++ install-winget.ps1
@@ -1,4 +1,4 @@
 #Install-Script winget-install -Force
 #winget-install
 &([ScriptBlock]::Create((irm winget.pro))) -Force
-winget install -e --id Dell.CommandUpdate --accept-package-agreements --silent
+winget install -e --id Dell.CommandUpdate --accept-package-agreements --accept-source-agreements --silent
