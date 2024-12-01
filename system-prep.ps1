2ecf2a2439f4d845ee01d4d3cca55bb78219e493
David Szpunar <dszp@users.noreply.github.com>
2024-12-01 15:54:04 -0500

    tweak system-prep docs

--- system-prep.ps1
+++ system-prep.ps1
@@ -37,8 +37,12 @@ Unblock-File *.ps1
 Write-Host "To return to this directory in an Administrative PowerShell window in the future, type: cd ~\quizsetup"
 Write-Host "The security hardening and Dell Command Update (if Dell computer) are the next important scripts to run."
 Write-Host "Only run the appropriate Windows Feature Update script if not yet at Windows Pro 22H2 or later in the System properties."
+Write-Host "To update to the latest setup file, while in this directory type: git pull"
 
-Write-Host "`nThe following files are in the $($env:USERPROFILE)\quizsetup directory:" -ForegroundColor Green
+Write-Host "`nThe following documentation file for reference are in the $($env:USERPROFILE)\quizsetup directory:" -ForegroundColor Green
+Get-ChildItem -Path "." -Filter "*.md" | Select-Object -Property Name # | Where-Object{ $_.Name -match ".*\.md$" }
+
+Write-Host "`nThe following script are in the $($env:USERPROFILE)\quizsetup directory:" -ForegroundColor Green
 Get-ChildItem -Path "." -Filter "*.ps1" | Select-Object -Property Name | Where-Object{ $_.Name -match "[0-9]-.*\.ps1$" }
 
 Write-Host "`nRun the scripts starting with 2-harden-windows.ps1 and so forth, some multiple times if needed." -ForegroundColor Green
