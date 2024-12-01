faaf8e46e4189f93b352f9dc10c7a56f2984cb11
David Szpunar <dszp@users.noreply.github.com>
2024-11-30 23:16:10 -0500

    tweak system-prep order and commands

--- system-prep.ps1
+++ system-prep.ps1
@@ -1,19 +1,11 @@
 # Disable Fastboot
-# /v is the REG_DWORD /t Specifies the type of registry entries /d Specifies the data for the new entry /f Adds or deletes registry content without prompting for confirmation.
-
-Write-Host "Disabling FastBoot via registry key."
+Write-Host "Disabling FastBoot via registry key..."
 REG ADD "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager\Power" /v HiberbootEnabled /t REG_DWORD /d "0" /f
 
 # Disable sleep and standby when on AC power:
-Write-Host "Disabling sleep and standby when on AC power."
+Write-Host "Disabling sleep and standby when on AC power..."
 Powercfg /Change monitor-timeout-ac 0
 Powercfg /Change standby-timeout-ac 0
-
-# Harden Windows Registry
-iex (irm "https://raw.githubusercontent.com/dszp/MSP-Scripts/refs/heads/main/Windows-Security-Hardening/Harden-Security-Windows-Registry.ps1")
-
-# Disable PowerShell 2 for security
-iex (irm "https://raw.githubusercontent.com/dszp/MSP-Scripts/refs/heads/main/Windows-Security-Hardening/Disable-PowerShell-V2.ps1")
 
 #Install-Script winget-install -Force
 #winget-install
@@ -32,3 +24,9 @@ git clone https://github.com/dszp/quiz-laptop-init.git .
 
 # Run in an Administrative PowerShell prompt manually against a specific file, this unblocks downloaded files to execute:
 Unblock-File *.ps1
+
+# Harden Windows Registry
+iex (irm "https://raw.githubusercontent.com/dszp/MSP-Scripts/refs/heads/main/Windows-Security-Hardening/Harden-Security-Windows-Registry.ps1" -Verbose:$false)
+
+# Disable PowerShell 2 for security
+iex (irm "https://raw.githubusercontent.com/dszp/MSP-Scripts/refs/heads/main/Windows-Security-Hardening/Disable-PowerShell-V2.ps1")
