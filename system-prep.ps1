8a5e2497a75ab4c31cc603549d4b07e046fe4b69
David Szpunar <dszp@users.noreply.github.com>
2024-11-30 23:17:28 -0500

    system-prep WinGet install change

--- system-prep.ps1
+++ system-prep.ps1
@@ -9,7 +9,7 @@ Powercfg /Change standby-timeout-ac 0
 
 #Install-Script winget-install -Force
 #winget-install
-&([ScriptBlock]::Create((irm winget.pro))) -Force
+&([ScriptBlock]::Create((irm winget.pro))) -Force -NoExit
 
 # Uncomment to install Dell Command Update via WinGet:
 # winget install -e --id Dell.CommandUpdate --accept-package-agreements --accept-source-agreements --silent
