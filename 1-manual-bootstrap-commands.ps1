075061128ead9b577559e586aa9c62719e3524a0
David Szpunar <dszp@users.noreply.github.com>
2024-11-30 23:35:20 -0500

    add bootstrap comment with next steps

--- 1-manual-bootstrap-commands.ps1
+++ 1-manual-bootstrap-commands.ps1
@@ -2,3 +2,20 @@
 
 & Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Force
 iex (irm "https://raw.githubusercontent.com/dszp/quiz-laptop-init/refs/heads/main/system-prep.ps1")
+
+<#
+After this script runs successfully, you should be able to launch an Administrative PowerShell 
+Prompt and type:
+
+cd ~/quizsetup
+dir
+
+This will list the files that can be run locally to finish setup processes.
+Run each file with a dot-backslash in front (tab-complete works), like:
+
+.\dcu-install-update.ps1
+
+To update the files, while inside the directory, type:
+
+git pull
+#>
