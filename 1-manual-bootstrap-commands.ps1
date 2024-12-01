40afff68641669c12e097462b92987152cc13267
David Szpunar <dszp@users.noreply.github.com>
2024-11-30 23:18:22 -0500

    force execution policy

--- 1-manual-bootstrap-commands.ps1
+++ 1-manual-bootstrap-commands.ps1
@@ -1,5 +1,4 @@
 # Paste this script into an Administrative PowerShell prompt to start system prep and copy scripts locally:
 
-& Set-ExecutionPolicy -ExecutionPolicy Unrestricted
+& Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Force
 iex (irm "https://raw.githubusercontent.com/dszp/quiz-laptop-init/refs/heads/main/system-prep.ps1")
-
