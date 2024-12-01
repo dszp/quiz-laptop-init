d1148fecf370ea6cd4c9495b8da726a6334b6a7a
David Szpunar <dszp@users.noreply.github.com>
2024-11-30 23:11:52 -0500

    fix typo in bootstrap command

--- 1-manual-bootstrap-commands.ps1
+++ 1-manual-bootstrap-commands.ps1
@@ -1,5 +1,5 @@
 # Paste this script into an Administrative PowerShell prompt to start system prep and copy scripts locally:
 
-& Set-Execution-Policy -ExecutionPolicy Unrestricted
+& Set-ExecutionPolicy -ExecutionPolicy Unrestricted
 iex (irm "https://raw.githubusercontent.com/dszp/quiz-laptop-init/refs/heads/main/system-prep.ps1")
 
